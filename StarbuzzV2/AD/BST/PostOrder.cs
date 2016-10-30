be4060d3de3ad631d97358fcb63b087a78b879b8
Robnibourg93 <robnibourg@hotmail.nl>
2016-10-30 23:43:21 +0100

    PostOrderTreeIterator

---
+++ StarbuzzV2/AD/BST/PostOrder.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarbuzzV2.AD.BST
+{
+    class PostOrder<T> : TreeIterator<T>
+    {
+        protected Stack<StNode> s;
+        protected class StNode {
+            BinaryNode<T> node;
+            int timesPopped;
+
+            public StNode(BinaryNode<T> n) {
+                Node = n; TimesPopped = 0;
+            }
+
+            public BinaryNode<T> Node
+            {
+                get
+                {
+                    return node;
+                }
+
+                set
+                {
+                    node = value;
+                }
+            }
+
+            public int TimesPopped
+            {
+                get
+                {
+                    return timesPopped;
+                }
+
+                set
+                {
+                    timesPopped = value;
+                }
+            }
+        }
+
+        public PostOrder(BinaryTree<T> theThree): base(theThree) {
+            s = new Stack<StNode>();
+            s.Push(new StNode(t.Root));
+        }
+
+        public override void advance()
+        {
+            if (s.Count == 0) {
+                if (current == null) {
+                    throw new Exception("No such element");
+                }
+                current = null;
+                return;
+            }
+
+            StNode cnode;
+            while (true) {
+                cnode = s.Pop();
+                if (++cnode.TimesPopped == 3) {
+                    current = cnode.Node;
+                    return;
+                }
+
+                s.Push(cnode);
+
+                if (cnode.TimesPopped == 1)
+                {
+                    if (cnode.Node.getleft() != null)
+                    {
+                        s.Push(new StNode(cnode.Node.getleft()));
+                    }
+                }
+                else
+                {
+                    if (cnode.Node.getRight() != null)
+                    {
+                        s.Push(new StNode(cnode.Node.getRight()));
+                    }
+                }
+            }
+
+        }
+
+        public override void first()
+        {
+            s.Clear();
+            if (t.Root != null) {
+                s.Push(new StNode(t.Root));
+                advance();
+            }
+        }
+    }
+}
---
+++ StarbuzzV2/AD/BST/TreeIterator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarbuzzV2.AD.BST
+{
+    abstract class TreeIterator<T>
+    {
+        protected BinaryTree<T> t;
+        protected BinaryNode<T> current;
+
+        public TreeIterator(BinaryTree<T> theTree){
+            t = theTree;
+            current = null;
+            }
+
+        public bool isValid() {
+            return current != null;
+        }
+
+        public T retrieve() {
+            if (current == null) {
+                throw new Exception("Element not found!");
+            }
+            return current.getElement();
+        }
+
+        abstract public void first();
+        abstract public void advance();
+    }
+}
